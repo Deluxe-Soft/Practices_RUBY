b9e5715a64ee1b6a75243916b3d578a57b5e4b54
mikolaj92 <mikpat92@gmail.com>
2014-07-21 14:22:24 +0200

    pierwsza proba programowania

--- zad1_1_patryk.rb
+++ zad1_1_patryk.rb
@@ -1,3 +1,34 @@
-def liczbadni(dzien, miesiac, rok)
-	
+class Zadanie_1
+	def liczbadni(dzien, miesiac, rok)
+		@dosylw = 365*wartosc_bezwzgledna(rok-2010)
+		#boże muszę się nad tym zastanowić
+		@liczbaDniMiesiąca = case miesiac
+			when 1 then 31
+			when 2
+				if rok%4==0
+					29
+				else 
+					28
+				end
+			when 3 then 31
+			when 4 then 30
+			when 5 then 31
+			when 6 then 30
+			when 7 then 31
+			when 8 then 31
+			when 9 then 30
+			when 10 then 31
+			when 11 then 30
+			when 12 then 31
+		end
+		@dosylw += liczbaDniMiesiąca*(1..(miesiac-1)
+		@dosylw += dzien-1
+		@dosylw
+
+
+	def wartosc_bezwzgledna(x)
+		return x if x >= 0
+		return -x if x < 0
+	end
 end
+
---
+++ zad2_1_patryk.rb
@@ -0,0 +1,8 @@
+class Zadanie_2
+  def sumaListyLiczb (listaliczb)
+  	for element in listaliczb
+  		suma += element
+  end
+
+  @lista = [1,2,3,4,5,6,7,8]
+end
