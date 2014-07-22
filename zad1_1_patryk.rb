dddc4f3feeee067a0b5e079c866f641d3fcecb8a
mikolaj92 <mikpat92@gmail.com>
2014-07-22 07:57:27 +0200

    Zmiana zadania 1

--- zad1_1_patryk.rb
+++ zad1_1_patryk.rb
@@ -1,34 +1,53 @@
 class Zadanie_1
-	def liczbadni(dzien, miesiac, rok)
-		@dosylw = 365*wartosc_bezwzgledna(rok-2010)
-		#boże muszę się nad tym zastanowić
-		@liczbaDniMiesiąca = case miesiac
-			when 1 then 31
+	def liczbaDniDoSylwestra(dzien, miesiac, rok)
+		wynik += (dzien-1)
+		(1..(miesiac-1)).each do |dni|
+			wynik+= liczbaDniMiesiaca(dni)
+		end
+		wynik += (365*(wartosc_bezwzgledna(rok-2010)))
+		
+	end
+
+
+	def wartosc_bezwzgledna(x)
+		x if x >= 0
+		-x if x < 0
+	end
+
+	def liczbaDniMiesiaca(miesiac)
+		liczbaDni = case miesiac
+			when 1 
+				31
 			when 2
 				if rok%4==0
 					29
 				else 
 					28
 				end
-			when 3 then 31
-			when 4 then 30
-			when 5 then 31
-			when 6 then 30
-			when 7 then 31
-			when 8 then 31
-			when 9 then 30
-			when 10 then 31
-			when 11 then 30
-			when 12 then 31
+			when 3
+				31
+			when 4
+				30
+			when 5
+				31
+			when 6
+				30
+			when 7
+				31
+			when 8
+				31
+			when 9
+				30
+			when 10
+				31
+			when 11
+				30
+			else 12
+				31
 		end
-		@dosylw += liczbaDniMiesiąca*(1..(miesiac-1)
-		@dosylw += dzien-1
-		@dosylw
-
-
-	def wartosc_bezwzgledna(x)
-		return x if x >= 0
-		return -x if x < 0
+		liczbaDni
 	end
+
+	liczbaDniDoSylwestra(21, 10, 2012)
 end
 
