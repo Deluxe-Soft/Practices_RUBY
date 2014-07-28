677d9182f3f331fc23a9308df98857872abb86fe
mikolaj92 <mikpat92@gmail.com>
2014-07-28 16:32:51 +0200

    Poprawione. Działa, ale słabo

--- zad3_2_patryk.rb
+++ zad3_2_patryk.rb
@@ -1,22 +1,22 @@
 
 class Classes
+  include ObjectSpace
+
   attr_accessor :hour, :people, :name
   attr_accessor :classroom
-
-  @@classes_instances = Array.new
-  @@classes_count = 0
-
   def initialize (hour, surname, name)
     @name=name
     @hour=hour
     @people = Array.new
     @people <<surname
-    @@classes_instances << self
-    @@classes_count +=1
   end
 
   def add_classroom(classroom)
     @classroom = classroom
+  end
+
+  def remove_classroom(classroom)
+    @classroom = nil
   end
 
   def add_person(surname)
@@ -27,73 +27,92 @@ class Classes
     self.people.delete(surname)
   end
 
-end
-
-#każde zajęcia mają listę na której znajdują się osoba, godzina
-
-class Classroom
-
-  attr_accessor :classroom_id
-  attr_accessor :classes
-
-  @@classroom_instances = Array.new
-  @@classroom_count = 0
-
-  def initialize (classroom_id, c)
-    @classroom_id=classroom_id
-    @classes = Array.new
-    @classes << c
-    @@classroom_instances << self
-    @@classroom_count +=1
-    raise if @classes.empty?
-
-  end
-
-  def add_classes(classes)
-    @classes<<classes
-  end
-
-
-  def remove_classes(classes)
-      @classes.delete(classes)
-  end
-end
-
-
-class Admin
-  attr_accessor :classes, :classroom
-   #TODO are classrooms valid
-  def are_classrooms_valid?
-    classes.classes_instances.all? do |iterator|
-      iterator.include?(classroom)
+  def is_person_already_added?(surname)
+    if self.people.include?(surname)
+      raise "This person is already added to this classes"
     end
   end
 
-  def is_class_valid?(classroom_obj) #obiekty SALA
-    Classes.@@classroom_instances.all? do |c|
-      if classroom_obj.classroom_id==c.classroom_id and classroom_obj.classes==c.classes
-        classroom_obj.hour!=c.hour
+  def is_hour_valid?(surname) 
+    ObjectSpace.each_object(Classes) do |c|
+      if c.people.include?(surname)
+        if self.hour != c.hour or self.id!=c.id
+          puts "Można się zapisać"
+        else 
+          raise "ERRORY zapisane już są chopki"
+        end
       end
     end
   end
 
-  def is_hour_valid?(classes_obj, surname) #class classes object + surname
-    @@classes_instances.all do |c|
-      c.people.include?(surname)
-      classes_obj.hour != c.hour and classes_obj.id!=c.id
+  def self.do_all_classes_have_clasrooms?
+    ObjectSpace.each_object(self) do |iterator|
+      if iterator.classroom==nil
+        raise "Nie wszystkie zajęcia mają przypisane sale"
+      end
     end
+  end
+
+end
+
+
+class Classroom
+  include ObjectSpace
+
+  attr_accessor :classroom_id
+  attr_accessor :classes
+  def initialize (classroom_id, c)
+    @classroom_id=classroom_id
+    @classes = Array.new
+    @classes << c
+    c.add_classroom(classroom_id)
+    raise if @classes.empty?
+  end
+
+  def add_classes(classes)
+    @classes<<classes
+    classes.add_classroom(self)
+  end
+
+
+  def remove_classes(classes)
+    @classes.delete(classes)
+    classes.remove_classroom(self)
+  end
+
+  def self.is_class_valid? #obiekty SALA
+    list = Array.new
+    ObjectSpace.each_object(Classroom) do |c|
+        if c.classes.size>1
+          c.classes.each do |x|
+            list <<x.hour
+          end
+        end
+      end
+      raise "errory nie valid bum" unless (list.uniq!)==nil
+
   end
 
 
 end
 
+
 zajecia1 = Classes.new(13,'Mateja','Matematyka')
 zajecia2 = Classes.new(15, 'Edwardowicz', 'Polski')
+zajecia3 = Classes.new(13, 'Luzak', 'Chinski')
 zajecia1.add_person('Mikolajczyk')
 zajecia1.add_person('Bednarek')
 sala1= Classroom.new(110,zajecia1)
+sala2= Classroom.new(111,zajecia3)
 sala1.add_classes(zajecia2)
-puts "\n #{sala1.classes}"
+puts "#{zajecia1.classroom}"
+puts "#{zajecia3.classroom}"
+zajecia1.is_hour_valid?('Edwardowicz')
+Classes.do_all_classes_have_clasrooms?
+Classroom.is_class_valid?
+
+
+
 
 
 
